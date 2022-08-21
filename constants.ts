50950563a5038d0ebdee55ddffdc62625280eac8
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2022-08-22 11:43:04 +1200

    🌯 first alpha run: use real help-desk

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 export const DAA_SERVER_ID = '690141234596937780'
 export const RESERVE_ALERTS_CHANNEL_ID = '1009585830848909373'
-export const HELP_DESK_CHANNEL_ID = '1009592156819816500'
+export const HELP_DESK_CHANNEL_ID = '999053532625903748'
 export const RESERVE_ROLE_ID = '1009586205832269938'
 
 // time before unresolved messages are sent to reserves
