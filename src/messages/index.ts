a008aa5eca11f99e82d49fe9c1b61527d580be00
Rohan Fowler-Harper <rohan.fowlerharper@gmail.com>
2023-03-13 11:08:48 +1300

    test: 💿 add debug logs

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -15,30 +15,44 @@ export async function setup(
     const { isValid, channel, categoryChannel } = validateChildChannel(
       message.channel
     )
+    console.log(isValid)
     if (!isValid) return
+    console.log('is valid')
     if (!isActiveCohort(categoryChannel)) return
+    console.log('is active cohort')
     if (channel.name !== HELP_DESK_NAME) return
+    console.log('is help desk')
     if (!isStudentMessage(message)) return
+
+    console.log('is help message')
 
     const unresolvedMessagesForCategory = unresolvedMessages.get(
       categoryChannel.id
     )
 
+    console.log(unresolvedMessages)
+
     if (!unresolvedMessagesForCategory) return
+    console.log('has category')
 
     const mentionedChannel = message.mentions.channels.first()
     const mentionedRole = message.mentions.roles.first()
 
     const isHelpMessage = () => {
+      console.log('checking mentionedChannel')
       if (mentionedChannel) return true
+      console.log('checking mentionedRole')
       if (mentionedRole) return true
 
+      console.log('checking for "help in"')
       if (message.content.toLowerCase().includes('help in')) return true
 
+      console.log('unchecked')
       return false
     }
 
     if (isHelpMessage()) {
+      console.log('is help message, adding to list...')
       unresolvedMessagesForCategory.set(message.id, {
         ...message,
         status: 'unresolved',
